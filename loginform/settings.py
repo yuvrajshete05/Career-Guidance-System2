4c80f66df806cb4f2092be7e77bd9098aa3dc719
yuvraj <yuvrajshete612@gmail.com>
2025-05-23 22:22:41 +0530

    Add Render domain to ALLOWED_HOSTS

--- loginform/settings.py
+++ loginform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wskoxr2q&xi&f7usfd!d710b8@q98(fm08y@2cg506$(vy(@*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
